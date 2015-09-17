94614d3c772bff87c731fc17a10c27e418cbc16f
Mantenimiento <Mantenimiento@192.168.31.4>
2015-09-17 11:28:15 +0200

    Versión 1709151128

--- Empresita/src/modelo/Modelo.java
+++ Empresita/src/modelo/Modelo.java
@@ -44,7 +44,7 @@ public class Modelo extends Database{
     public DefaultTableModel getTablaClientes(int id){
         DefaultTableModel tablemodel = new DefaultTableModel();
         int registros = 0;
-        String[] columNames = {"Nombre", "Apellidos", "Servicio", "Precio"};
+        String[] columNames = {"Nombre", "Apellidos", "DNI", "Servicio", "Precio"};
         try{
             PreparedStatement pstm = this.getConexion().prepareStatement("SELECT count(*) as total FROM Clientes WHERE ID_Trabajador = '"+id+"'");
             ResultSet res = pstm.executeQuery();
@@ -54,16 +54,17 @@ public class Modelo extends Database{
         }catch(SQLException e){
             System.err.println(e.getMessage());
         }
-        Object[][] data = new String[registros][2];
+        Object[][] data = new String[registros][5];
         try{
-            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT Nombre, Apellidos, Servicio, Precio FROM Clientes WHERE ID_Trabajador = '"+id+"'");
+            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT Nombre, Apellidos, Dni, Servicio, Precio FROM Clientes WHERE ID_Trabajador = '"+id+"'");
             ResultSet res = pstm.executeQuery();
             int i=0;
             while(res.next()){
                 data[i][0] = res.getString("Nombre");
                 data[i][1] = res.getString("Apellidos");
-                data[i][2] = res.getString("Servicio");
-                data[i][3] = res.getString("Precio");
+                data[i][2] = res.getString("Dni");
+                data[i][3] = res.getString("Servicio");
+                data[i][4] = res.getString("Precio");
                 i++;
             }
             res.close();
@@ -88,7 +89,21 @@ public class Modelo extends Database{
         return id;
     }
     
-    public boolean NuevoTrabajador(String nombre, String apellidos, String dni, String servicios){
+    public int idCliente(String dni){
+        int id = 0;
+        String q = "SELECT ID_Cliente FROM Clientes WHERE Dni = '"+dni+"'";
+        try {
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            ResultSet res = pstm.executeQuery();
+            id = res.getInt("ID_Cliente");
+            pstm.close();
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return id;
+    }
+    
+    public boolean nuevoTrabajador(String nombre, String apellidos, String dni, String servicios){
         
         int maxId = 0;
         String q1 = "SELECT max(ID_Trabajador) as ID_Trabajador FROM Trabajadores";
@@ -116,7 +131,7 @@ public class Modelo extends Database{
         return false;
     }
     
-    public boolean NuevoCliente(String nombre , String apellidos, String servicio, double precio){
+    public boolean nuevoCliente(String nombre , String apellidos, String dni, String servicio, double precio, int idT){
         
         int maxId = 0;
         String q1 = "SELECT max(ID_Cliente) as ID_Cliente FROM Clientes";
@@ -131,8 +146,8 @@ public class Modelo extends Database{
         
         maxId++;
         
-        String q2 = "INSERT INTO Clientes (ID_Cliente, Nombre, Apellidos, Servicio, Precio) "
-                    + "VALUES ("+maxId+", '"+nombre+"', '"+apellidos+"', '"+servicio+"', "+precio+")";
+        String q2 = "INSERT INTO Clientes (ID_Cliente, Nombre, Apellidos, Dni, Servicio, Precio, ID_Trabajador) "
+                    + "VALUES ("+maxId+", '"+nombre+"', '"+apellidos+"', '"+dni+"', '"+servicio+"', "+precio+", "+idT+")";
         try {
             PreparedStatement pstm = this.getConexion().prepareStatement(q2);
             pstm.execute();
@@ -142,5 +157,49 @@ public class Modelo extends Database{
             System.err.println( e.getMessage() );
         }
         return false;
+    }
+    
+    public boolean eliminarTrabajador(String id){
+        boolean res=false;
+        String q = "DELETE FROM Trabajadores WHERE ID_Trabajador = "+id;
+        try {
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            pstm.execute();
+            pstm.close();
+            res=true;
+        }catch(SQLException e){
+            System.err.println( e.getMessage() );
+        }
+        return res;
+    }
+    
+    public boolean eliminarCliente(String id){
+        boolean res=false;
+        String q = "DELETE FROM Trabajadores WHERE ID_Trabajador = "+id;
+        try {
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            pstm.execute();
+            pstm.close();
+            res=true;
+        }catch(SQLException e){
+            System.err.println( e.getMessage() );
+        }
+        return res;
+    }
+    
+    public String[] infoTrabajador(String dni){
+        String[] info = new String[4];
+        try{
+            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT Nombre, Apellidos, Dni, Servicios FROM Trabajadores WHERE Dni = '"+dni+"'");
+            ResultSet res = pstm.executeQuery();
+            info[0] = res.getString("Nombre");
+            info[1] = res.getString("Apellidos");
+            info[2] = res.getString("Dni");
+            info[3] = res.getString("Servicios");
+            res.close();
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return info;
     }
 }
