73fc20aeb1edcb6dd2f54990142be40758f8d530
Mantenimiento <Mantenimiento@192.168.31.4>
2015-10-19 13:54:38 +0200

    Versión 1910151354

--- Proyecto SQLite/src/modelo/database.java
+++ Proyecto SQLite/src/modelo/database.java
@@ -1,11 +1,13 @@
 package modelo;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-public class database {
+public class database{
     
     private String bd;
     private Connection con;
@@ -15,11 +17,22 @@ public class database {
     
     private void conexionBD(){
         try{
-            String controlados = "org.sqlite.JDBC";//Definir
-            Class.forName(controlados).newInstance();
+            String controlador = "org.sqlite.JDBC";
+            Class.forName(controlador).newInstance();
             System.out.println("Conexión realizada con éxito");
         }catch(ClassNotFoundException | InstantiationException | IllegalAccessException e){
-            JOptionPane.showMessageDialog(null,"Error al carga el controlador.");
+            JOptionPane.showMessageDialog(null,"Error al cargar el controlador.");
+        }
+    }
+    
+    private void cargarbd(){
+        try{
+            bd = "jdbc:sqlite:---------------------"; //Aquí he de introducir la ruta de la base de datos local
+            con = DriverManager.getConnection(bd,null,null);
+            System.out.println("BD cargada con éxito.");
+
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"Error al cargar la BD.");
         }
     }
 }
