ba11497590e44a55ef43ec48bb56ee0bb12c29ef
Nacho <Nacho@192.168.1.42>
2015-11-17 22:44:15 +0100

    Versión 1711152244

---
+++ Proyecto1DI/src/controlador/Main.java
@@ -0,0 +1,11 @@
+
+package controlador;
+
+import vista.Interfaz;
+
+public class Main {
+    
+    public static void main(String[] args){
+        new Controlador(new Interfaz()).iniciar();
+    }
+}
