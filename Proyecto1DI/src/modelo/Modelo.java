443bdfa4c64ccb3c41e02ef389bb8a6468b109de
Mantenimiento <Mantenimiento@192.168.31.4>
2015-11-18 09:54:08 +0100

    Versión 1811150954

--- Proyecto1DI/src/modelo/Modelo.java
+++ Proyecto1DI/src/modelo/Modelo.java
@@ -1,5 +1,10 @@
 package modelo;
 
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class Modelo extends Database{
@@ -9,5 +14,49 @@ public class Modelo extends Database{
         public boolean isCellEditable(int row, int column){  
             return false;  
         }
+    }
+    
+    public String[] getProveedores(){
+        int registros = 0;
+        try{
+            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT count(CIF) as total FROM Proveedores");
+            ResultSet res = pstm.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al contar tuplas\n" + e.getMessage());
+        }
+        String[] proveedores = new String[registros];
+        try{
+          
+            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT CIF FROM Proveedores");
+            ResultSet res = pstm.executeQuery();
+            int i=0;
+            while(res.next()){
+                proveedores[i] = res.getString("CIF");
+            i++;
+            }
+            res.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al obtener datos\n" + e.getMessage());
+        }
+        return proveedores;
+    }
+    
+    public String getNombreProveedor(String cif){
+        String nombre = "";
+        try{
+            String q = "SELECT Nombre FROM Proveedores WHERE CIF = '"+cif+"'";
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            ResultSet res = pstm.executeQuery();
+            while(res.next()){
+                nombre = res.getString("Nombre");
+            }
+            res.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al obtener el nombre\n" + e.getMessage());
+        }
+        return nombre;
     }
 }
