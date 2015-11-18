58916ffd3ca1dd81fdfce40c2e65348d02dfa154
Mantenimiento <Mantenimiento@192.168.31.4>
2015-11-18 10:35:22 +0100

    Versión 1811151035

--- Proyecto1DI/src/modelo/Modelo.java
+++ Proyecto1DI/src/modelo/Modelo.java
@@ -1,6 +1,5 @@
 package modelo;
 
-import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,11 +25,12 @@ public class Modelo extends Database{
             res.close();
         }catch(SQLException e){
             JOptionPane.showMessageDialog(null, "Error al contar tuplas\n" + e.getMessage());
+            e.printStackTrace();
         }
         String[] proveedores = new String[registros];
         try{
-          
-            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT CIF FROM Proveedores");
+            String q = "SELECT CIF FROM Proveedores";
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
             ResultSet res = pstm.executeQuery();
             int i=0;
             while(res.next()){
@@ -39,7 +39,8 @@ public class Modelo extends Database{
             }
             res.close();
         }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Error al obtener datos\n" + e.getMessage());
+            JOptionPane.showMessageDialog(null, "Error al obtener datos\n\n" + e.getMessage());
+            e.printStackTrace();
         }
         return proveedores;
     }
@@ -55,8 +56,112 @@ public class Modelo extends Database{
             }
             res.close();
         }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Error al obtener el nombre\n" + e.getMessage());
+            JOptionPane.showMessageDialog(null, "Error al obtener el nombre\n\n" + e.getMessage());
+            e.printStackTrace();
         }
         return nombre;
+    }
+    
+    public DefaultTableModel tablaProveedores(){
+        DefaultTableModel tablemodel = new ModeloTablaNoEditable();
+        int registros = 0;
+        String[] columNames = {"CIF","Nombre","Dirección","Teléfono","Correo"};
+        try{
+            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT count(CIF) as total FROM Proveedores");
+            ResultSet res = pstm.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al contar tuplas\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        Object[][] data = new String[registros][5];
+        try{
+            String q = "SELECT CIF, Nombre, Direccion, Telefono, Correo FROM Proveedores";
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            ResultSet res = pstm.executeQuery();
+            int i=0;
+            while(res.next()){
+                data[i][0] = res.getString("CIF");
+                data[i][1] = res.getString("Nombre");
+                data[i][2] = res.getString("Direccion");
+                data[i][3] = res.getString("Telefono");
+                data[i][4] = res.getString("Correo");
+            i++;
+            }
+            res.close();
+            tablemodel.setDataVector(data, columNames);
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al obtener datos\n\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        return tablemodel;
+    }
+    
+    public DefaultTableModel tablaProveedoresVacia(){
+        DefaultTableModel tablemodel = new ModeloTablaNoEditable();
+        int registros = 0;
+        String[] columNames = {"CIF","Nombre","Dirección","Teléfono","Correo"};
+        Object[][] data = new String[registros][5];
+        try{
+            tablemodel.setDataVector(data, columNames);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error al construir tabla\n\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        return tablemodel;
+    }
+    
+    public DefaultTableModel tablaClientes(){
+        DefaultTableModel tablemodel = new ModeloTablaNoEditable();
+        int registros = 0;
+        String[] columNames = {"DNI","Nombre","Apellidos","Dirección","Teléfono","Correo"};
+        try{
+            PreparedStatement pstm = this.getConexion().prepareStatement("SELECT count(DNI) as total FROM Clientes");
+            ResultSet res = pstm.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al contar tuplas\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        Object[][] data = new String[registros][6];
+        try{
+            String q = "SELECT DNI, Nombre, Apellidos, Direccion, Telefono, Correo FROM Clientes";
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            ResultSet res = pstm.executeQuery();
+            int i=0;
+            while(res.next()){
+                data[i][0] = res.getString("DNI");
+                data[i][1] = res.getString("Nombre");
+                data[i][2] = res.getString("Apellidos");
+                data[i][3] = res.getString("Direccion");
+                data[i][4] = res.getString("Telefono");
+                data[i][5] = res.getString("Correo");
+            i++;
+            }
+            res.close();
+            tablemodel.setDataVector(data, columNames);
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al obtener datos\n\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        return tablemodel;
+    }
+    
+    public DefaultTableModel tablaClientesVacia(){
+        DefaultTableModel tablemodel = new ModeloTablaNoEditable();
+        int registros = 0;
+        String[] columNames = {"DNI","Nombre","Apellidos","Dirección","Teléfono","Correo"};
+        Object[][] data = new String[registros][6];
+        try{
+            tablemodel.setDataVector(data, columNames);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error al construir tabla\n\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        return tablemodel;
     }
 }
