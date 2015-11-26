c37a0ddab8385d17917462156d0a4e8f0a5cf37b
jairo <jairo@MrPcOp>
2015-11-26 22:56:09 +0100

    Version 2611152257

--- Proyecto1DI/src/controlador/PDF.java
+++ Proyecto1DI/src/controlador/PDF.java
@@ -1,6 +1,39 @@
-
 package controlador;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import modelo.Modelo;
+
 public class PDF {
-    
+
+    Modelo consultas = new Modelo();
+
+    public void generarFacturaPDF() throws FileNotFoundException, DocumentException {
+        Document documento = new Document();
+        FileOutputStream fos = new FileOutputStream("Factura.pdf");
+        PdfPTable tablaEmpresa, tablaPedidos, tablaArticuloPedido;
+
+        consultas.tablaArticulosPedidos(null);
+
+        PdfWriter.getInstance(documento, fos).setInitialLeading(20);
+
+        documento.open();
+        //Creamos las tablas
+        tablaEmpresa = new PdfPTable(5);
+        tablaPedidos = new PdfPTable(5);
+        tablaArticuloPedido = new PdfPTable(4);
+
+        documento.add(new Paragraph("AQUI EL CLIENTE"));
+        documento.add(new Paragraph());
+
+    }
+
+    public void generarProformaPDF() {
+
+    }
 }
